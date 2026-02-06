f8a3c104129d3998f4bab19e36c74fb34338de2f
KacperStrugala1 <kacperstrugala5@gmail.com>
2026-02-06 22:22:55 +0100

    adding files to form

--- data_leaks/data_leaks/views.py
+++ data_leaks/data_leaks/views.py
@@ -1,4 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from . import forms, services
 
 def index(request):
-    return HttpResponse("Hllo")
+    if request.method == "POST":
+        form = forms.UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = form.cleaned_data['file']
+            cleaned_file = services.get_metadata(file)
+            return HttpResponse(f"{cleaned_file}")
+    else:
+        form = forms.UploadFileForm()
+    return render(request, "home.html", {"form":form})

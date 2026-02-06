074acf5718a0502a76f84a9abfe6072b9b927119
KacperStrugala1 <kacperstrugala5@gmail.com>
2026-02-06 22:39:54 +0100

    Added logging configuration

--- data_leaks/data_leaks/views.py
+++ data_leaks/data_leaks/views.py
@@ -1,14 +1,18 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from . import forms, services
+from loguru import logger
 
 def index(request):
-    if request.method == "POST":
-        form = forms.UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            file = form.cleaned_data['file']
-            cleaned_file = services.get_metadata(file)
-            return HttpResponse(f"{cleaned_file}")
-    else:
-        form = forms.UploadFileForm()
-    return render(request, "home.html", {"form":form})
+    try:
+        if request.method == "POST":
+            form = forms.UploadFileForm(request.POST, request.FILES)
+            if form.is_valid():
+                file = form.cleaned_data['file']
+                cleaned_file = services.get_metadata(file)
+                return HttpResponse(f"{cleaned_file}")
+        else:
+            form = forms.UploadFileForm()
+        return render(request, "home.html", {"form":form})
+    except Exception as exc:
+        logger.error(f"Error {exc}")

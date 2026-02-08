68f5741eeed5da31f53e18de7b3a5bee2aeb6a4a
KacperStrugala1 <kacperstrugala5@gmail.com>
2026-02-08 23:50:28 +0100

    added new photo and file metadata

--- data_leaks/data_leaks/file_type.py
+++ data_leaks/data_leaks/file_type.py
@@ -1,21 +1,21 @@
 from .services import PdfFile, PhotoFile
 from PIL import Image
-
+from django.http import HttpResponse
 import logging
 
 class FileType:
     def __init__(self):
         self.pdf_file = PdfFile()
         self.image_file = PhotoFile()
-        image_extensions = {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".ico"}  
-        pdf_extension = ".pdf"
+        #image_extensions = {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".ico"}  
+        #pdf_extension = ".pdf"
 
     def check_file_format(self, file):
         content_type = file.content_type
 
         if content_type == "application/pdf":
-            self.pdf_file.get_metadata(file)
+            return self.pdf_file.get_metadata(file)
         elif content_type in ["image/jpeg", "image/png"]:
-            self.image_file.get_metadata(file)
+            return self.image_file.get_metadata(file)
         else:
-            logging.error("Unsupported file type")
+            return "Unsupported file type"
--- data_leaks/data_leaks/views.py
+++ data_leaks/data_leaks/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import forms
 from .services import PdfFile, PhotoFile
 import logging
-from . import file_type
+from .file_type import FileType
 
 def index(request):
     try:
@@ -14,13 +14,19 @@ def index(request):
             if form.is_valid():
                 
                 file = form.cleaned_data['file']
-                file_format = file_type.FileType()
-                meta_data = file_format.check_file_format(file)
+                file_type = FileType()
+                meta_data = file_type.check_file_format(file)
+                logging.debug(meta_data)
     
-                return HttpResponse(f"{meta_data}")
+                return redirect("meta")
         else:
             logging.debug("not valid")
             form = forms.UploadFileForm()
         return render(request, "home.html", {"form":form})
+    
     except Exception as exc:
         logging.error(f"Error {exc}")
+        return HttpResponse(f"Internal error: {exc}", status=500)
+
+def meta(request):
+    return render(request, "meta.html")

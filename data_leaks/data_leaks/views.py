458707903e6c3c0391138b28399aead1f5ec1b23
kstrugal <kacper.strugala@nokia.com>
2026-02-10 16:16:28 +0100

    added html views and better ui look

--- data_leaks/data_leaks/views.py
+++ data_leaks/data_leaks/views.py
@@ -1,32 +1,38 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, redirect
+from django.views.generic import ListView
 from . import forms
 from .services import PdfFile, PhotoFile
 import logging
 from .file_type import FileType
 
-def index(request):
-    try:
-        if request.method == "POST":
-            form = forms.UploadFileForm(request.POST, request.FILES)
-            logging.debug(request.FILES)
-            logging.debug(request.POST)
-            if form.is_valid():
-                
-                file = form.cleaned_data['file']
-                file_type = FileType()
-                meta_data = file_type.check_file_format(file)
-                logging.debug(meta_data)
-    
-                return redirect("meta")
-        else:
-            logging.debug("not valid")
-            form = forms.UploadFileForm()
-        return render(request, "home.html", {"form":form})
-    
-    except Exception as exc:
-        logging.error(f"Error {exc}")
-        return HttpResponse(f"Internal error: {exc}", status=500)
+class IndexView(ListView):
+    def get(self, request):
+        try:
+            if request.method == "POST":
+                form = forms.UploadFileForm(request.POST, request.FILES)
+                logging.debug(request.FILES)
+                logging.debug(request.POST)
+                if form.is_valid():
+                    
+                    file = form.cleaned_data['file']
+                    file_type = FileType()
+                    meta_data = file_type.check_file_format(file)
 
-def meta(request):
-    return render(request, "meta.html")
+                    return render(request, "meta_view.html", {"meta_data":meta_data})
+            else:
+                logging.debug("not valid")
+                form = forms.UploadFileForm()
+            return render(request, "home.html", {"form":form})
+        
+        except Exception as exc:
+            logging.error(f"Error {exc}")
+            return HttpResponse(f"Internal error: {exc}", status=500)
+    def post(self, request):
+        pass
+
+def meta_view(request):
+    if request.method == "POST":
+
+        return JsonResponse()
+    return render(request, "meta_view.html")

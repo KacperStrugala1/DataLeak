27e9eca405886ff7399ae92a61d3626f1b570acf
kstrugal <kacper.strugala@nokia.com>
2026-02-10 16:16:28 +0100

    added html views and better ui look

--- data_leaks/data_leaks/urls.py
+++ data_leaks/data_leaks/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="home_view"),
-    path("meta/", views.meta, name="meta"),
+    path("", views.IndexView.asView(), name="home_view"),
+    path("meta_view/", views.meta_view, name="meta_view"),
     path('admin/', admin.site.urls),
 ]
---
+++ to_do_list.md
@@ -0,0 +1,9 @@
+
+1. Listing metadata in meta.html view
+2. Showin metadata in JSON
+3. Downloading new file without metas
+4. README on website
+5. Dockerize
+6. unitests
+7. flake8 
+8. maybe deploy or smt

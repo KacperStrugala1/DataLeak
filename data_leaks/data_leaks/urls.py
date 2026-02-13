4cc0322f82e2faaba55c4046b0437d8c5040c6e4
kstrugal <kacper.strugala@nokia.com>
2026-02-13 22:02:45 +0100

    change to CBV

--- data_leaks/data_leaks/urls.py
+++ data_leaks/data_leaks/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import View
 from . import views
 
+
 urlpatterns = [
-    path("", views.index, name="home_view"),
-    path("meta_view/", views.meta_view, name="meta_view"),
-    path("valentine/", views.valentine, name="valentine"),
+    path("", views.HomeView.as_view(), name="home_view"),
+    path("meta_view/", views.MetaView.as_view(), name="meta_view"),
+    #path("valentine/", views.valentine, name="valentine"),
     path('admin/', admin.site.urls),
 ]

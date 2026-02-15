ffa95fe522768669f902c9ed1a58d9250bdac93c
KacperStrugala1 <kacperstrugala5@gmail.com>
2026-02-15 21:26:35 +0100

    added new handling and downloading files

--- data_leaks/data_leaks/views.py
+++ data_leaks/data_leaks/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404, HttpResponseServerError
 from django.shortcuts import render, redirect
 from django.views import View
 
@@ -7,8 +7,9 @@ from .file_type import FileType
 
 import logging
 import json
+import os
 
-
+logger = logging.getLogger(__name__)
 
 class HomeView(View):
     form_class = forms.UploadFileForm
@@ -19,15 +20,17 @@ class HomeView(View):
         return render(request, self.template_name, {"form": form})
 
     def post(self, request):
-            try:
+        action = request.POST.get("action")
 
+        if action == "show_metadata":
+            try:
                 form = self.form_class(request.POST, request.FILES)
                 if form.is_valid():
                     file = form.cleaned_data['file']
                     file_type = FileType()
                     meta_data = file_type.check_file_format(file)
 
-                    #sending meta_data in session
+                    #sending meta_data in session to meta_view
                     request.session['meta_data'] = meta_data
                     return redirect("meta_view")
                 else:
@@ -35,18 +38,48 @@ class HomeView(View):
                     return render(request, "home.html", {"form":form})
                 
             except Exception as exc:
-                logging.error(f"Error {exc}")
+                logger.error(f"Error {exc}")
                 return HttpResponse(f"Internal error: {exc}", status=500)
-        
+            
+        elif action == "download_without_meta":
+                form = self.form_class(request.POST, request.FILES)
+
+                if form.is_valid():
+                    file = form.cleaned_data['file']
+                    
+                    file_type = FileType()
+                    cleared_file = file_type.delete_file(file)
+                try:
+                    response = HttpResponse(
+                        cleared_file.getvalue(),
+                        content_type='application/octet-stream'
+                    )
+                    response["Content-Disposition"] = (
+                        f'attachment; filename={file.name}.pdf'
+                    )
+                    return response
+                
+                except Exception as exc:
+                    logger.error(f"Error {exc}")
+                    return HttpResponse(f"Error: {exc}")
+        else:
+            logger.info("Wrong operation")
+            return HttpResponse("Wrong operation")
+
 
 class MetaView(View):
     template_name = "meta_view.html"
 
     def get(self, request):
-        meta_data = request.session.get("meta_data")
+        try:
+            meta_data = request.session.get("meta_data")
             # del request.session["meta_data"]
-        if not meta_data:
-            return HttpResponse("Invalid session get or no metadata")
+            if not meta_data:
+                raise Http404("No metadata in session")
+        except Exception as exc:
+            logger.exception(f"Error with getting data session. Error {exc}")
+            return HttpResponseServerError("Server error occured")
+        
         return render(request, self.template_name, {"meta_data":meta_data})
 
     def post(self, request):
@@ -56,6 +89,7 @@ class MetaView(View):
 
             if action == "show_json":
                 return JsonResponse(meta_data, json_dumps_params={'ensure_ascii':False})
+            
             elif action == "download_json":
                 response = HttpResponse(
                 json.dumps(meta_data, ensure_ascii=False), 
@@ -63,17 +97,31 @@ class MetaView(View):
                 )
                 response["Content-Disposition"] = 'attachment; filename="file.json"'
                 return response
+            
+            elif action == "download_clear_file":
+                form = self.form_class(request.POST, request.FILES)
+
+                if form.is_valid():
+                    file = form.cleaned_data['file']
+                    
+                    file_type = FileType()
+                    cleared_file = file_type.delete_file(file)
+                try:
+                    response = HttpResponse(
+                        cleared_file.getvalue(),
+                        content_type='application/octet-stream'
+                    )
+                    response["Content-Disposition"] = f'attachment; filename={file.name}.pdf'
+                    return response
+                
+                except Exception as exc:
+                    logger.error(f"Error {exc}")
+                    return HttpResponse(f"Error: {exc}")
             else:
                 if not meta_data:
-                    return HttpResponse("Invalid session get or no metadata")
+                    return Http404("Invalid session process or get no metadata")
+           
         
         except Exception as exc:
-            return HttpResponse(f"Error: {exc}")
-    
-    
-def delete_meta_data(request):
-    try:
-        if request.method == "POST":
-            pass
-    except Exception as exc:
-        return HttpResponse(f"Error: {exc}")
+            logger.exception(f"Error with getting data session. Error {exc}")
+            return HttpResponseServerError("Server error occured")
